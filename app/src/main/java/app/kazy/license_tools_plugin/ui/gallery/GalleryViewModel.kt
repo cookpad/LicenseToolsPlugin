bd0b73ea3eb3ba5653c2adcd2238e1aa9d99c15a
kazuki-yoshida <kazuki-yoshida@cookpad.com>
2019-10-08 18:14:25 +0900

    Initial commit

---
+++ app/src/main/java/app/kazy/license_tools_plugin/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package app.kazy.license_tools_plugin.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/app/kazy/license_tools_plugin/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package app.kazy.license_tools_plugin.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
