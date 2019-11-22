70bd3577d07022659fd92dc19092d34796cbe875
kazuki-yoshida <kazuki-yoshida@cookpad.com>
2019-11-22 13:54:58 +0900

    Setup test env

--- plugin/src/test/java/app/kazy/plugin/LicenseToolsPluginTest.kt
+++ plugin/src/test/java/app/kazy/plugin/LicenseToolsPluginTest.kt
@@ -4,7 +4,7 @@ import org.junit.Test
 
 import org.junit.Assert.*
 
-class ExampleUnitTest {
+class LicenseToolsPluginTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
