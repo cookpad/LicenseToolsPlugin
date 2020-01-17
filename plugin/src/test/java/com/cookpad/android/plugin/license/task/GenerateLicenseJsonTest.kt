791ed867c1f80c248a015670d9b62c7c85a96b1d
kazuki-yoshida <kazuki-yoshida@cookpad.com>
2020-01-17 18:14:25 +0900

    Fix output json style

--- plugin/src/test/java/com/cookpad/android/plugin/license/task/GenerateLicenseJsonTest.kt
+++ plugin/src/test/java/com/cookpad/android/plugin/license/task/GenerateLicenseJsonTest.kt
@@ -7,8 +7,10 @@ import org.junit.Test
 class GenerateLicenseJsonTest {
     @Test
     fun toJson_isSuccess() = GenerateLicenseJson.run {
-        val yamlInfoList = loadYamlFromResources("yaml/licenses.yml")
+        val yamlInfoList = loadYamlFromResources("yaml/example_apache2_license.yml")
         val htmlString = yamlInfoList.toJson()
-        assertThat(htmlString).isNotEmpty()
+        assertThat(htmlString).isEqualTo("""
+            {"libraries":[{"artifactId":{"name":"example-common","group":"com.example","version":"+"},"copyrightHolder":"Example Author","copyrightStatement":"Copyright &copy; Example Author. All rights reserved.","license":"The Apache Software License, Version 2.0","licenseUrl":"http://www.apache.org/licenses/LICENSE-2.0.txt","normalizedLicense":"apache2","url":"https://example.com/example/example-common","libraryName":"example-common"}]}
+        """.trimIndent())
     }
 }
